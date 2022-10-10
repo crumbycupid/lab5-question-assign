057af14c920bb37e53d1336a4da4354a8093b495
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2022-10-10 17:19:55 -0400

    new changes

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {//eslint-disable-line
-  let product = a * b; 
+  let product = a * b;
   let message = `The product of ${a} and ${b} is ${product}.`;
   return [product, message];
 }
